cd1bf1da1b0483302f92ec987e466358fae7a886
JangasCodingplace <janisgoesser92@gmail.com>
2022-03-24 22:28:14 +0100

    Add filebeat to docker composition

--- app/main.py
+++ app/main.py
@@ -1,6 +1,30 @@
+import logging
+import sys
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi import status
 from models import Location, Speed
+
+from settings import fun, LOG_DIR
+
+
+logger = logging.getLogger()
+
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.DEBUG)
+log_format = logging.Formatter(
+    "%(asctime)s %(levelname)-8s %(name)-8s %(message)s",
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+console_handler.setFormatter(log_format)
+logger.addHandler(console_handler)
+
+Path.mkdir(LOG_DIR, exist_ok=True)
+file_logger = logging.FileHandler(LOG_DIR / "app.log")
+file_logger.setFormatter(log_format)
+logger.addHandler(file_logger)
 
 
 app = FastAPI()
@@ -8,6 +32,8 @@ app = FastAPI()
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def get_health():
+    logger.info("Health Check Requested")
+    fun()
     return {"message": "ok"}
 
 
--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from pathlib import Path
 from confluent_kafka import Producer, SerializingProducer
 from confluent_kafka.schema_registry import SchemaRegistryClient
@@ -7,11 +8,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
+LOG_DIR = BASE_DIR / "logs"
 
 KAFKA_PRODUCER = Producer(**{
     "bootstrap.servers": os.environ["KAFKA_HOST"]
 })
 
 SCHEMA_REGISTRY_URL = os.environ["SCHEMA_REGISTRY_URL"]
+
+
+def fun():
+    logger.info("This is a logging test")
